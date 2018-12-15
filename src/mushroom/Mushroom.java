b79a7023ab9b9673f6e4f1bd7ac225bf118d2ba2
Unknown <jose.nuez96@gmail.com>
2018-12-15 17:15:33 -0300

    Lector de archivo
    Se añade el lector del archivo .data y un split para obtener un arreglo con los datos

--- src/mushroom/Mushroom.java
+++ src/mushroom/Mushroom.java
@@ -4,7 +4,10 @@
  * and open the template in the editor.
  */
 package mushroom;
-
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.Scanner;
 /**
  *
  * @author Roberto Ureta
@@ -15,7 +18,20 @@ public class Mushroom {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        try{
+            String linea;
+            FileReader f = new FileReader(new File("test.txt"));
+            BufferedReader b = new BufferedReader(f);
+            while((linea = b.readLine())!=null) {
+                System.out.println(linea);
+                linea = linea.replace("\t", "");
+                linea = linea.trim();
+                String[] arregloLinea = linea.split(",");
+            }
+
+        }catch(Exception e){
+            System.out.println("Error al abrir el archivo");
+        }
     }
     
 }
