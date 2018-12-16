dff51a36f16e0388edab326093959783b7641a2d
Diego <diego.iturriaga.p@gmail.com>
2018-12-16 18:22:09 -0300

    Clase RedNeuronal

---
+++ src/mushroom/RedNeuronal.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mushroom;
+
+/**
+ *
+ * @author Diego
+ */
+public class RedNeuronal {
+    
+    double[][] data = new double[8124][23];//todos los datos codificados
+    double[][] entrenamiento = new double[6905][23];//85% de los datos
+    double[][] prueba = new double[1219][23];//15 % de los datos
+    public RedNeuronal() {
+    }
+    
+}
